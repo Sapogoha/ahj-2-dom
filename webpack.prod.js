1c199ea4559a06cbf9662e6e59109d2f8efdcbeb
Aleksey Sokolov <sokolov.alexey@me.com>
2022-04-14 16:10:18 +0300

    fixed webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
-// const TerserPlugin = require('terser-webpack-plugin');
+// const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
@@ -8,5 +8,4 @@ module.exports = merge(common, {
   optimization: {
     minimizer: [`...`, new CssMinimizerPlugin()],
   },
-  plugins: [new MiniCssExtractPlugin()],
 });
